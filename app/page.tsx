090d97bb23b8849a6a5ad0d8505f9241221272f8
Mohammad Asrar <113281948+asrar211@users.noreply.github.com>
2026-01-26 15:05:34 +0530

    first

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </nav>
 
       <div className="grid xl:grid-cols-2 grid-cols-1 xl:max-w-7xl xl:mx-auto mx-10">
-          <div className="flex flex-col mt-20">
+          <div className="flex flex-col mt-10">
             <span className="bg-neutral-300 w-fit text-xs font-semibold px-1 rounded-full text-green-600 flex items-center"><Dot/>HEALTH CHECK</span>
             <h1 className="mt-10 text-5xl font-semibold text-green-600 leading-14">Empathetic <br/>Mental Health <br/><span className="text-green-800">Chatbot</span></h1>
             <p className="text-center mt-2 text-neutral-500 text-sm">This Chatbot is only for educational purposes and is not a substitute for professional help.</p>
