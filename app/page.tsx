66356f7bfadd76bb353db7367006ee8ad60457f7
Mohammad Asrar <113281948+asrar211@users.noreply.github.com>
2026-01-26 15:08:10 +0530

    first

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-dvh">
-      <nav className="flex mt-4 mx-10 justify-between items-center bg-neutral-300/50 rounded-full text-black p-4">
+      <nav className="flex mt-4 mx-2 xl:max-w-7xl xl:mx-auto justify-between items-center bg-neutral-300/50 rounded-full text-black p-4">
         <div className="flex gap-2">
           <MessageCircleDashed className="cursor-pointer"/>
           <Phone className="cursor-pointer"/>
@@ -13,16 +13,18 @@ export default function Home() {
         <Menu className="cursor-pointer"/>
       </nav>
 
-      <div className="grid xl:grid-cols-2 grid-cols-1 xl:max-w-7xl xl:mx-auto mx-10">
+      <div className="grid xl:grid-cols-2 grid-cols-1 xl:max-w-7xl xl:mx-auto mx-2">
           <div className="flex flex-col mt-10">
             <span className="bg-neutral-300 w-fit text-xs font-semibold px-1 rounded-full text-green-600 flex items-center"><Dot/>HEALTH CHECK</span>
-            <h1 className="mt-10 text-5xl font-semibold text-green-600 leading-14">Empathetic <br/>Mental Health <br/><span className="text-green-800">Chatbot</span></h1>
+            <h1 className="mt-10 text-5xl text-center font-semibold text-green-600 leading-14">Empathetic <br/>Mental Health <br/><span className="text-green-800">Chatbot</span></h1>
             <p className="text-center mt-2 text-neutral-500 text-sm">This Chatbot is only for educational purposes and is not a substitute for professional help.</p>
-            <Link href="/login"><button className="border border-neutral-600 w-fit mt-10 flex justify-center items-center px-4 py-2 rounded-full  text-sm cursor-pointer">Try it out</button></Link>
+            <div className="flex justify-center items-center">
+              <Link href="/login"><button className="border border-neutral-600 w-fit mt-10 flex justify-center items-center px-4 py-2 rounded-full  text-sm cursor-pointer">Try it out</button></Link>
+            </div>
           </div>
       </div>
 
-      <div className="flex justify-center items-center mx-10 text-center mt-10">
+      <div className="flex justify-center items-center mx-2 text-center mt-10">
         <p className="text-sm font-semibold">“An AI-assisted psychological self-assessment platform designed to help users understand potential mental health patterns for awareness purposes only — not for medical diagnosis or treatment.”</p>
       </div>
 
